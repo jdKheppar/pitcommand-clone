9f51a8cabf36178ce8950c06e862f7482656cf6a
mohammad junaed <mohammadjunaed858@gmail.com>
2023-08-06 18:21:38 +0500

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let hamburger = document.getElementById("hamburger-icon");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
 });
+
+window.onscroll = function () {
+  // Check if the hamburger has the "open" class
+  if (!hamburger.classList.contains("open")) {
+    myFunction();
+  }
+};
+
+var header = document.getElementById("nav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
